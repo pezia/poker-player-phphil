951f6dd002c400e675362b2b4bd19dc8e1098ed1
Zsolt Petrik <pezia@pezia.hu>
2014-12-06 10:04:24 +0100

    Small index.php refactor

--- index.php
+++ index.php
@@ -4,14 +4,22 @@ require_once('player.php');
 
 $player = new Player();
 
-switch($_POST['action'])
-{
+$gameState = null;
+
+if (isset($_POST['game_state'])) {
+    $gameState = json_decode($_POST['game_state'], true);
+}
+
+switch ($_POST['action']) {
     case 'bet_request':
-        echo $player->betRequest(json_decode($_POST['game_state'], true));
+        echo $player->betRequest($gameState);
         break;
     case 'showdown':
-        $player->showdown(json_decode($_POST['game_state'], true));
+        $player->showdown($gameState);
         break;
+    case 'check':
     case 'version':
+    default:
         echo Player::VERSION;
+        break;
 }
