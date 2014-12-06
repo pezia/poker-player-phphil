842e926ae9bea7355ec12e7c45c85306b51d5a31
Zsolt Petrik <pezia@pezia.hu>
2014-12-06 10:20:13 +0100

    Game state fix

--- player.php
+++ player.php
@@ -5,7 +5,7 @@ class Player {
     const VERSION = "PHP Player 0.1";
 
     public function betRequest($gameState) {
-        return $gameState->current_buy_in + 50;
+        return $gameState['current_buy_in'] + 50;
     }
 
     public function showdown($gameState) {
