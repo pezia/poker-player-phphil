306dcab70a50b443ccb9a86cf6cae5d2d28cbbfe
Balázs Sándor <sandorbalazszsolt@gmail.com>
2014-12-06 11:07:18 +0100

    a

--- src/Model/Card.php
+++ src/Model/Card.php
@@ -6,9 +6,18 @@ class Card {
     public $suit;
     public $rank;
 
+    public $ranks = array(
+        2,3,4,5,6,7,8,9,10,
+        'J','Q','K','A',
+    );
+
     public function __construct($suit, $rank) {
         $this->suit = $suit;
         $this->rank = $rank;
+    }
+
+    public function getRankScore() {
+        return array_search($this->rank, $this->ranks);
     }
 
 } 
--- tests/acceptance/BasicCest.php
+++ tests/acceptance/BasicCest.php
@@ -1,29 +1,24 @@
 <?php
 use \AcceptanceTester;
 
-class BasicCest
-{
+class BasicCest {
     private $gameState;
 
-    public function _before(AcceptanceTester $I)
-    {
+    public function _before(AcceptanceTester $I) {
         $json = file_get_contents('tests/_data/gamestate.json');
         $this->gameState = preg_replace('~//.*$~m', '', $json);
     }
 
-    public function _after(AcceptanceTester $I)
-    {
+    public function _after(AcceptanceTester $I) {
     }
 
-    // tests
-    public function tryToTest(AcceptanceTester $I)
-    {
+    public function check(AcceptanceTester $I) {
         $I->sendPOST('/', array('action' => 'check'));
         $I->seeResponseCodeIs(200);
     }
-    public function betRequest(AcceptanceTester $I)
-    {
-        $I->sendPOST('/', array('action' => 'bet_request', 'game_state'=> $this->gameState));
+
+    public function betRequest(AcceptanceTester $I) {
+        $I->sendPOST('/', array('action' => 'bet_request', 'game_state' => $this->gameState));
         $I->seeResponseCodeIs(200);
         $I->seeResponseEquals(370);
     }
